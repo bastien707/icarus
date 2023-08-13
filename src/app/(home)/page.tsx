aa4aeb378d71820a86f6f8a603086026f2475a20
bastien707 <bastien.facqueur@efrei.net>
2023-08-13 18:25:34 +0200

    feat: rainbowkit wallet connection

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,23 @@
-export default async function Home() {
+"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+  const { address, isConnected } = useAccount();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
     <main>
       <h1 className="text-2xl">Home</h1>
+      <ConnectButton />
+      {!isMounted && <p>Loading...</p>}
+      {isMounted && isConnected && <p>Connected with address: {address}</p>}
     </main>
   );
 }
