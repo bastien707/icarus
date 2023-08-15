f22c861c460192862367770a2a0cf7e57fdf560f
bastien707 <bastien.facqueur@efrei.net>
2023-08-15 15:35:16 +0200

    fix: responsive height not fully displayed

--- src/app/(home)/register/page.tsx
+++ src/app/(home)/register/page.tsx
@@ -2,8 +2,8 @@ import RegisterForm from "./register";
 
 export default async function SignUp() {
   return (
-    <div className="w-screen flex justify-center items-center flex-col">
-      <div className="sm:w-1/2 lg:w-1/3 p-10 gap-4 border-2 border-black duration-500 bg-white shadow-[10px_10px_0px_0px_rgba(0,0,0)] ">
+    <div className="h-fit w-screen flex justify-center items-center flex-col">
+      <div className="sm:w-1/2 lg:w-1/3 p-10 my-10 gap-4 border-2 border-black duration-500 bg-white shadow-[10px_10px_0px_0px_rgba(0,0,0)] ">
         <RegisterForm title='Create your account'/>
       </div>
     </div>
