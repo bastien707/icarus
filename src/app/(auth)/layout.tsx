d118e13d27620cafaff7c76c8030e1b42b906e91
bastien707 <bastien.facqueur@efrei.net>
2023-08-16 20:50:56 +0200

    feat: add image background

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,11 +10,7 @@ const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={spaceGrotesk.className}>
@@ -22,7 +18,7 @@ export default function RootLayout({
           className="w-full h-full fixed top-0 left-0 bg-no-repeat bg-cover -z-50"
           style={{ backgroundImage: "url(/loginBackground.svg)" }}
         ></div>
-        <header className="mt-4 ml-4">
+        <header className="mt-4 ml-4 w-fit">
           <Link href={route.HOME}>
             <BiChevronLeft size={48} />
           </Link>
