93ffcc5c364ddb13ca152dd7132aeecc7b36b58a
bastien707 <bastien.facqueur@efrei.net>
2023-08-17 15:54:59 +0200

    feat: add background color

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 export default async function Home() {
   return (
-    <main>
+    <main className="bg-icarus-light-purple h-screen">
       <h1 className="text-2xl">This is the home page !</h1>
     </main>
   );
