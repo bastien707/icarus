009007585d0e427476cc269f7b535a909b922dcc
bastien707 <bastien.facqueur@efrei.net>
2023-08-17 10:52:46 +0200

    "fix: merge conflict on RootLayer"

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,14 @@
-import "../globals.css";
-import { Space_Grotesk } from "next/font/google";
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
-import type { Metadata } from "next";
-import { Providers } from "./providers";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import '../globals.css';
+import { Space_Grotesk } from 'next/font/google';
+import { Navbar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
+import type { Metadata } from 'next';
+import { Providers } from './providers';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -16,19 +16,14 @@ export const metadata: Metadata = {
   description: 'Icarus rise finance',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
-  
+
   return (
     <html lang="en">
       <body className={spaceGrotesk.className}>
         <Providers>
-          <Navbar session={session}/>
+          <Navbar session={session} />
           {children}
           <Footer />
         </Providers>
