720879faae10bc4806836b90facb657b88845905
bastien707 <bastien.facqueur@efrei.net>
2023-08-17 20:28:50 +0200

    feat: use PropWithChildren

--- src/app/(app)/providers.tsx
+++ src/app/(app)/providers.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { PropsWithChildren } from 'react';
 
-type Props = {
-  children: React.ReactNode;
-};
-
-export const Providers = ({ children }: Props) => {
+export const Providers = ({ children }: PropsWithChildren) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
